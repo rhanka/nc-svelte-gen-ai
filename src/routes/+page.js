f7896ee5863ba02c638eafa22c826e35eca8d6b9
antoinefa <fabien.antoine@m4x.org>
2025-01-03 22:37:02 -0500

    enable spa rendering with npm run build

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,3 +2,5 @@
 // Make sure to test whether or not you can re-enable it, as SSR improves perceived performance and site accessibility.
 // Read more about this option here: https://svelte.dev/docs/kit/page-options#ssr
 export const ssr = false;
+export const prerender = true;
+
