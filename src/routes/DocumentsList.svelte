0774352518b7a722db5ed6c318021b63b29918da
antoinefa <fabien.antoine@m4x.org>
2025-01-05 10:54:46 -0500

    move md ref to pdf

--- src/routes/DocumentsList.svelte
+++ src/routes/DocumentsList.svelte
@@ -1,41 +1,42 @@
 <script>
     import { createEventDispatcher } from 'svelte';
     import { marked } from 'marked'; // Import the marked library
-  
+
     export let documentsList = [];
     const dispatch = createEventDispatcher();
     let searchQuery = '';
-  
+
     function selectDoc(doc) {
-      dispatch('doc', { doc });
+      dispatch('selectDoc', { doc });
     }
-  
+
   </script>
-  
+
   <div>
 		{#if documentsList.length > 0}
 			<button on:click={() => documentsList = []} style="width: 100%;padding: 0.3rem;background-color: #cecece; border: none; cursor: pointer;">
         Clean AI Filter
       </button>
-		{/if}		
+		{/if}
   </div>
 
+  <div class="scrollable">
+    <ul style="list-style-type: none; padding: 0;">
+      {#each documentsList as doc, index}
+        <li style="padding: 0; border-bottom: 1px solid #ccc; list-style-type: none;">
+          <button
+            type="button"
+            on:click={() => selectDoc(doc)}
+            on:keypress={(e) => e.key === 'Enter' && selectDoc(doc)}
+            style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;">
+            <strong>{doc.doc.replace(/\.md$/,'.pdf')}  </strong>
+            <p style="margin-top:0.2rem;margin-bottom:0rem;">{doc.chunk.slice(0,100)}...</p>
+          </button>
+        </li>
+      {/each}
+    </ul>
+  </div>
 
-  <ul style="list-style-type: none; padding: 0;">
-		{#each documentsList as doc, index}
-      <li style="padding: 0; border-bottom: 1px solid #ccc; list-style-type: none;">
-        <button 
-          type="button" 
-          on:click={() => selectDoc(doc.doc)} 
-          on:keypress={(e) => e.key === 'Enter' && selectDoc(doc)} 
-          style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;">
-          <strong>{doc.doc}  </strong>
-          <p>{doc.chunk.slice(0,100)}...</p>
-        </button>
-      </li>
-    {/each}
-  </ul>
-  
   <style>
     li:hover {
       background-color: #f0f0f0;
@@ -45,4 +46,24 @@
 			margin-left: -8px!important;
 			font-size: 0.9rem;
 		}
+
+    .scrollable {
+		max-height: 20vh;
+		overflow-y: auto;
+    overflow-x: hidden;
+	}
+
+	.scrollable::-webkit-scrollbar {
+		width: 12px; /* Width of the scrollbar */
+	}
+
+	.scrollable::-webkit-scrollbar-track {
+		background: #f1f1f1; /* Background of the scrollbar track */
+	}
+
+	.scrollable::-webkit-scrollbar-thumb {
+		background: #888; /* Color of the scrollbar thumb */
+		border-radius: 10px; /* Rounded corners for the scrollbar thumb */
+	}
+
   </style>
