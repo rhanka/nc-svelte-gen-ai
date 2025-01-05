7838c7361469f81b67c9935739702d345e73f03c
antoinefa <fabien.antoine@m4x.org>
2025-01-05 10:55:51 -0500

    refacto with store

--- src/routes/NonConformityDetail.svelte
+++ src/routes/NonConformityDetail.svelte
@@ -1,18 +1,12 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
     import NonConformityTask from './NonConformityTask.svelte';
 
     export let selectedItem;
-    const dispatch = createEventDispatcher();
-  
-    function closeDetail() {
-      dispatch('closeDetail');
-    }
-  
+
     // Directive to inject HTML
     function html(node, htmlContent) {
       node.innerHTML = htmlContent;
-  
+
       return {
         update(newHtml) {
           node.innerHTML = newHtml;
@@ -20,35 +14,29 @@
       };
     }
   </script>
-  
+
     <div style="padding-left: 16px;padding-right: 8px; background-color: #f9f9f9;">
-			<button 
-          type="button" 
-          on:click={closeDetail} 
-          on:keypress={(e) => e.key === 'Enter' && closeDetail} 
+			<button
+          type="button"
           style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;">
 	      <h2>
 		  		<span	style="margin-left:-16px; padding: 0 8px; color: #aaa" title="close"></span>
 					{selectedItem['ATA_category']} - {selectedItem['nc_event_id']} - {selectedItem['nc_event_date'].replace(/:..\..*/,"")}
 				</h2>
-				
+
 			</button>
 	    <p><strong>{selectedItem['analysis_history']['000'][0]['label']}</strong> </p>
       <h3>Status History:</h3>
-				 <NonConformityTask expand={true} task={"Non-Conformity Report"} history={selectedItem['analysis_history']['000']}/>
-				 <NonConformityTask task={"Task 100 - Analysis"} history={selectedItem['analysis_history']['100']}/>
-				 <NonConformityTask task={"Task 200 - Analysis Validation"} history={selectedItem['analysis_history']['200']}/>
-				 <NonConformityTask task={"Task 300 - Stress Analysis"} history={selectedItem['analysis_history']['300']}/>
-				 <NonConformityTask task={"Task 400 - Stress Analysis Validation"} history={selectedItem['analysis_history']['400']}/>
-				 <NonConformityTask expand={true} task={"Task 500 - Final Analysis Validation"} history={selectedItem['analysis_history']['500']}/>
-					
+        {#each ['000', '100', '200', '300', '400', '500'] as task}
+          <NonConformityTask
+            expand={['000','500'].includes(task)}
+            task={task}
+            history={selectedItem['analysis_history']['000']}
+          >
+          </NonConformityTask>
+        {/each}
 		</div>
-    <div style="padding-left: 16px;padding-right: 8px; text-align: right; background-color: #f9f9f9;">
-      <button on:click={closeDetail} style="padding: 0.5rem 1rem; background-color: #6200ee; color: white; border: none; border-radius: 4px; cursor: pointer;">
-        Back to List
-      </button>
-    </div>
-  
+
   <style>
     li:hover {
       background-color: #f0f0f0;
--- src/routes/NonConformityList.svelte
+++ src/routes/NonConformityList.svelte
@@ -1,17 +1,18 @@
 <script>
     import { createEventDispatcher } from 'svelte';
-  
+
     export let nonConformities = [];
-		export let num = 0;
-		export let nonConformitiesFilter = [];
-		let filteredItems = [];
+	export let num = 0;
+	export let nonConformitiesFilter = [];
+
+	let filteredItems = [];
     const dispatch = createEventDispatcher();
     let searchQuery = '';
-  
+
     function selectItem(item) {
       dispatch('select', { item });
     }
-   
+
     // Filter items based on the search
     $: filteredItems = nonConformities.filter(item => {
 	      const query = searchQuery.toLowerCase();
@@ -21,61 +22,79 @@
 	        JSON.stringify(item['analysis_history']).toLowerCase().includes(query) // Added description to the search
 	      );
 	    }).filter(item => {
-				if (nonConformitiesFilter.length > 0) {
-					return nonConformitiesFilter.some(n => n.doc === item['nc_event_id'])
-				} else {
-					return true;
-				} 
-			})
-			.sort((a, b) => new Date(b['nc_event_date']) - new Date(a['nc_event_date']));;
+			if (nonConformitiesFilter.length > 0) {
+				return nonConformitiesFilter.some(n => n.doc === item['nc_event_id'])
+			} else {
+				return true;
+			}
+		});
 		$: num = filteredItems.length;
 
 		$: console.log(nonConformitiesFilter)
   </script>
-  
-  <div style="align-content:center">
+
+  <div>
+	{#if nonConformitiesFilter.length === 0}
 	    <input
 	      id="search"
 	      type="text"
 	      bind:value={searchQuery}
 	      placeholder="Search by ID, category, or description"
 	    />
-		{#if nonConformitiesFilter.length > 0}
-			<button 
-				on:click={() => nonConformitiesFilter = []}
-				style="width: 100%;padding: 0.3rem;background-color: #cecece; border: none; cursor: pointer;"
-				>
-        Clean AI Filter
-      </button>
-		{/if}
-
-		
+	{:else}
+		<button
+			on:click={() => nonConformitiesFilter = []}
+			style="width: 100%;padding: 0.3rem;background-color: #cecece; border: none; cursor: pointer;"
+		>
+        	Clean AI Filter
+      	</button>
+	{/if}
+  </div>
+  <div class="scrollable">
+	<ul style="list-style-type: none; padding: 0;">
+			{#each filteredItems as item, index}
+		<li style="padding: 0; border-bottom: 1px solid #ccc; list-style-type: none;">
+			<button
+			type="button"
+			on:click={() => selectItem(item)}
+			on:keypress={(e) => e.key === 'Enter' && selectItem(item)}
+			style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;">
+			<strong>{item['ATA_code']} - {item['ATA_category']} - {item['nc_event_date'].slice(0, 10)}  </strong>
+			<p style="margin-top:0.5rem;margin-bottom:0">{item['analysis_history']['000'][0]['label'].slice(0, 100)}...</p>
+			</button>
+		</li>
+		{/each}
+	</ul>
   </div>
 
-
-  <ul style="list-style-type: none; padding: 0;">
-		{#each filteredItems as item, index}
-      <li style="padding: 0; border-bottom: 1px solid #ccc; list-style-type: none;">
-        <button 
-          type="button" 
-          on:click={() => selectItem(item)} 
-          on:keypress={(e) => e.key === 'Enter' && selectItem(item)} 
-          style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;">
-          <strong>{item['ATA_code']} - {item['ATA_category']} - {item['nc_event_date'].slice(0, 10)}  </strong>
-          <p>{item['analysis_history']['000'][0]['label'].slice(0, 100)}...</p>
-        </button>
-      </li>
-    {/each}
-  </ul>
-  
   <style>
     li:hover {
       background-color: #f0f0f0;
     }
-		input {
-			padding: 0.35rem!important;
-			margin-bottom: 1rem;
-			width: 285px;
-			font-size: 0.9rem;
-		}
+	input {
+		padding: 0.35rem!important;
+		margin-bottom: 1rem;
+		width: 285px;
+		font-size: 0.9rem;
+	}
+	.scrollable {
+		max-height: 20vh;
+		overflow-y: auto;
+		width:100%;
+	}
+
+	.scrollable::-webkit-scrollbar {
+		width: 12px; /* Width of the scrollbar */
+	}
+
+	.scrollable::-webkit-scrollbar-track {
+		background: #f1f1f1; /* Background of the scrollbar track */
+	}
+
+	.scrollable::-webkit-scrollbar-thumb {
+		background: #888; /* Color of the scrollbar thumb */
+		border-radius: 10px; /* Rounded corners for the scrollbar thumb */
+	}
+
+
   </style>
