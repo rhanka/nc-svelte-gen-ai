7eeba5bc4c92c3bbbe899c4f68f63ca8793fac54
antoinefa <fabien.antoine@m4x.org>
2025-01-05 10:53:21 -0500

    add pdf viewer

---
+++ src/routes/ShowDocument.svelte
@@ -0,0 +1,12 @@
+<svelte:options runes={false} />
+<script>
+	import PdfViewer from 'svelte-pdf';
+
+    export let url = null;
+
+    $: console.log('pdf',url);
+</script>
+
+{#if url}
+    <PdfViewer url={url} />
+{/if}
