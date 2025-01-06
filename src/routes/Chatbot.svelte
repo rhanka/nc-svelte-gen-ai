a099e1ebf6414986e73dc82fdddb1ad45422bda7
antoinefa <fabien.antoine@m4x.org>
2025-01-05 20:18:06 -0500

    update chatbot style, adds input propoals

--- src/routes/Chatbot.svelte
+++ src/routes/Chatbot.svelte
@@ -4,10 +4,10 @@
 	import { DeepChat } from "deep-chat";
 	let aiUrl = 'https://dataiku.genai-cgi.com/web-apps-backends/NONCONFORMITIES/3DGvs3v/ai';
 	export let referencesList = [];
-	import { createdItem, updateCreatedItem } from './store.js'
+	import { createdItem, updateCreatedItem, isUpdating, askForHelp } from './store.js';
+	let chatElementRef;
 
   const history = [
-    { role: "system", text: "How can I help you today?" }
   ];
 
 	const responseInterceptor = async (response) => {
@@ -19,6 +19,7 @@
 			referencesList = json.sources;
 			let role = $createdItem.currentTask;
 			console.log('response',json);
+			$isUpdating = false;
 			$updateCreatedItem = { role: role, label: json.label, description: json.description};
 			return {html: marked(json.text)}; // Retourner le JSON � DeepChat
     } catch (e) {
@@ -57,44 +58,108 @@ I'm editing current task: ${JSON.stringify($createdItem['analysis_history'][role
 Please helping me the base update for this task and following specific request: ${requestDetails.body.messages[0].text}
 `
 		console.log('request', requestDetails)
-	  return requestDetails;
+		$isUpdating = role;
+	  	return requestDetails;
 	};
 </script>
 
 <main style="margin: -4px;">
   <deep-chat
-		avatars="true"
-    connect={{
-	    url: aiUrl,
-	    method: "POST",
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json',
-				'Origin': 'https://svelte.dev'
-			}
-	  }}
-    textInput={
-			{
-	    "styles": {
-	      "text": {"color": "black"},
-	      "container": {
+		bind:this={chatElementRef}
+		avatars={{
+			"ai": "https://upload.wikimedia.org/wikipedia/commons/e/ef/ChatGPT-Logo.svg"
+		}}
+		connect={{
+			url: aiUrl,
+			method: "POST",
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+					'Origin': 'https://svelte.dev'
+				}
+		}}
+		textInput={
+				{
+			"styles": {
+			"text": {"color": "black"},
+			"container": {
 					"width": "100%",
 					"border-radius": "0px",
 					"border-color": "#555"
 				},
-	      "focus": {"border": "2px solid #a2a2ff"}
-	    },
+			"focus": {"border": "2px solid #ccc"}
+			},
 			placeholder:{text: "Ask your question"}}
-		 }
+		}
+		intropanel={{
+			"styles": {
+			"container": {
+				"backgroundColor": "rgba(247,247,248)",
+				"borderTop": "1px solid rgba(0,0,0,.1)",
+				"borderBottom": "1px solid rgba(0,0,0,.1)"
+			},
+			"header": {
+				"color": "black"
+			},
+			"body": {
+				"color": "black"
+			}
+			},
+			"header": "Welcome to the Non-Conformity Chatbot",
+			"body": "I'm here to help you with your non-conformity tasks. Please provide me with the necessary information to help you."
+		}}
+		messageStyles={{
+			"default": {
+			"shared": {
+				"bubble": {
+				"maxWidth": "100%", "backgroundColor": "unset", "marginTop": "10px", "marginBottom": "10px"}},
+			"user": {
+				"bubble": {
+				"color": "black"}},
+			"ai": {
+				"outerContainer": {
+				"backgroundColor": "rgba(247,247,248)", "borderTop": "1px solid rgba(0,0,0,.1)", "borderBottom": "1px solid rgba(0,0,0,.1)"
+				}
+			}
+			}
+		}}
 		responseInterceptor={responseInterceptor}
-	  requestInterceptor={requestInterceptor}
-    history={history}
+	  	requestInterceptor={requestInterceptor}
+    	history={history}
 		chatStyle={{
 			"border": "0px",
 			"width": "300px",
 			"height": "250px"
 		}}
-  ></deep-chat>
+		htmlClassUtilities = {{
+			'custom-button': {
+				events: {
+				click: (event) => {
+					const text = event.target.children[0].innerText;
+					chatElementRef.submitUserMessage(text);
+				},
+				},
+				styles: {
+				default: {backgroundColor: '#f2f2f2', borderRadius: '10px', padding: '10px', cursor: 'pointer', textAlign: 'center'},
+				hover: {backgroundColor: '#ebebeb'},
+				click: {backgroundColor: '#e4e4e4'},
+				},
+			},
+			'custom-button-text': {styles: {default: {pointerEvents: 'none'}}},
+			}
+		}
+  >
+    <div style="display: none">
+    <div class="custom-button">
+      <div class="custom-button-text">Propose task description</div>
+    </div>
+    <div class="custom-button" style="margin-top: 15px">
+      <div class="custom-button-text">Translate to french</div>
+    </div>
+  </div>
+
+
+  </deep-chat>
 </main>
 
 <style>
