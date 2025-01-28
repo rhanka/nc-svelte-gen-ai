7fadef0b3c3b517841fa8b4e5b5beb160011750d
antoinefa <fabien.antoine@m4x.org>
2025-01-27 21:56:53 -0500

    add drawer

---
+++ src/routes/Drawer.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let expand = false;
+  function MWCDrawer (node) {
+		function handleNav(event) {
+			console.log(node, event.target);
+            expand = !expand;
+            node.open = expand;
+		}
+  }
+</script>
+
+<div
+  class="MuiDrawer-root MuiDrawer-docked MuiDrawer-root-custom"
+  style="display: {expand ? 'block' : 'none'}"
+>
+  <div
+    class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft MuiDrawer-custom"
+    style="transform: none; transition: transform 225ms cubic-bezier(0, 0, 0.2, 1);"
+  >
+    <nav
+      class="MuiList-root MuiList-padding"
+      aria-labelledby="nested-list-subheader"
+    >
+      <slot />
+    </nav>
+  </div>
+</div>
+
+<style>
+  .MuiDrawer-root-custom .MuiDrawer-paper {
+    box-sizing: border-box;
+    top: 75px;
+    width: 20rem;
+    box-shadow: none;
+    filter: drop-shadow(rgba(104, 114, 116, 0.267) 0px 2px 5px);
+    border-left: 1px solid rgb(214, 217, 219);
+    transition: display 0.3s ease 0s;
+    margin-left: 5rem;
+    background: rgb(248, 248, 248);
+    z-index: 200;
+  }
+
+  .MuiDrawer-custom {
+    background-color: rgb(255, 255, 255);
+    color: rgba(0, 0, 0, 0.87);
+    box-shadow: none;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    z-index: 1200;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1);
+    flex: 1 0 auto;
+    outline: 0px;
+    border-right: 1px solid rgba(0, 0, 0, 0.12);
+  }
+</style>
