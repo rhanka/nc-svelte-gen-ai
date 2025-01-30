3341e6e1b144625740ed7c264b1f42ea8ef12efd
Fabien ANTOINE <fabien.antoine@m4x.org>
2025-01-30 13:22:08 -0500

    add tiptap editor component to replace ugly textarea

---
+++ src/routes/TipTap.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { onMount, onDestroy } from 'svelte';
+	import { Editor } from '@tiptap/core';
+	import StarterKit from '@tiptap/starter-kit';
+    import { Markdown } from 'tiptap-markdown';
+
+    export let value;
+	let element;
+	let editor;
+
+    $: if (editor)  {
+        editor.commands.setContent(value);
+    }
+
+	onMount(() => {
+		editor = new Editor({
+			element: element,
+			extensions: [
+                StarterKit,
+                Markdown
+                ],
+			content: value,
+            onUpdate: ({ editor }) => {
+                const content = editor.storage.markdown.getMarkdown();
+                if (value !== content) {
+                    value = content;
+                }
+            },
+			onTransaction: () => {
+				// force re-render so `editor.isActive` works as expected
+				editor = editor;
+			},
+		});
+	});
+
+	onDestroy(() => {
+		if (editor) {
+			editor.destroy();
+		}
+	});
+</script>
+
+<div bind:this={element} />
+
+<style>
+	:global(.ProseMirror-focused) {
+        border: none!important;
+        outline: none!important;
+    }
+</style>
