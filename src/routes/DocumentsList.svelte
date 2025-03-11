8ad6a0c6cfe9272beae3e2191fc3e3fe0f5a51b2
Fabien Antoine <fabien.antoine@m4x.org>
2025-03-11 06:22:53 -0400

    dont truncate DocumentsList.svelte

--- src/routes/DocumentsList.svelte
+++ src/routes/DocumentsList.svelte
@@ -32,12 +32,7 @@
           style="cursor: pointer; padding: 8px; width: 100%; text-align: left; border: none; background: none;border-bottom: 1px solid rgba(0,0,0,.1)"
         >
           <strong
-            >{doc.doc.replace(/\.md$/, ".pdf").slice(0, 50)}{doc.doc.replace(
-              /\.md$/,
-              ".pdf",
-            ).length > 50
-              ? "..."
-              : ""}
+            >{doc.doc.replace(/\.md$/, ".pdf")}
           </strong>
           <p style="margin-top:0.2rem;margin-bottom:0rem;">
             {doc.chunks[0].chunk.slice(0, 100)}...
